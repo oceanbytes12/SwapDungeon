6d47e2f52fb48ff9bd82fbd0dab761b0914cb05a
AlexM <134806248+oceanbytes12@users.noreply.github.com>
2024-01-18 12:38:15 -0800

    Working on SM

---
+++ States/EnemyFollow.gd
@@ -0,0 +1,20 @@
+extends State
+class_name EnemyFollow
+
+@export var enemy: CharacterBody2D
+@export var move_speed := 40.0
+@export var player: CharacterBody2D
+
+func Enter():
+	pass
+
+func Physics_Update(delta: float):
+	var direction = player.global_position - enemy.global_position
+	
+	if direction.length() > 25:
+		enemy.velocity = direction.normalized() * move_speed
+	else:
+		enemy.velocity = Vector2()
+	
+	if direction.length() > 50:
+		Transitioned.emit(self, "idle")
