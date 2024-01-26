d55b9aed0f0ef05d3e8b62cbb350b6f1dd300611
AlexM <134806248+oceanbytes12@users.noreply.github.com>
2024-01-26 13:41:24 -0800

    Summons boys

--- Michael WIP/Scenes/HeroSpawner.gd
+++ Michael WIP/Scenes/HeroSpawner.gd
@@ -4,6 +4,8 @@ extends Node
 @export var spawnPositions: Array[Node2D]
 @export var partyPanels: Array[HeroUIPanel]
 @onready var PartyParent = $Party
+@onready var level = preload("res://Scene/Level1.tscn")
+@onready var MeleeSkeleton = preload("res://Scenes/Meleer.tscn")
 signal move_To_Ready_Screen
 
 func _GrabHerosFromUI():
@@ -16,6 +18,7 @@ func _GrabHerosFromUI():
 	return ret
 
 func _LoadHeros():
+	var newNode = level.instantiate()
 	var heros = _GrabHerosFromUI()
 	for heroEnumIndex in heros.size():
 		var currentEnum = heros[heroEnumIndex]
@@ -24,13 +27,15 @@ func _LoadHeros():
 			continue
 		
 		#Swap this out for a reference to a real hero!
-		var newHero = Globals._GetIconInstanceOfType(currentEnum)
-		
+		var newHero = MeleeSkeleton.instantiate()
+		newHero.teamColor = "blue"
+		newHero.controllable = true
 		#Position them properly
-		newHero.z_index = -1000
-		PartyParent.add_child(newHero)
+		newNode.add_child(newHero)
 		newHero.global_position = spawnPositions[heroEnumIndex].global_position
 		print("Spawning Hero at: ", newHero.global_position)
+	
+	get_parent().get_parent().add_child(newNode)
 
 func _FinishPartyAddition():
 	print("Finishing Addition")
@@ -39,4 +44,4 @@ func _FinishPartyAddition():
 func _UnLoadHeros():
 	for n in PartyParent.get_children():
 		PartyParent.remove_child(n)
-		n.queue_free() 
+		n.queue_free()
--- Scenes/Levels/AlexTesting/AlexTesting.gd
+++ Scenes/Levels/AlexTesting/AlexTesting.gd
@@ -6,10 +6,11 @@ var is_Ctrl_pressed = false
 # Get all units within the bounds of the drawn rectangle
 func get_units_in_area(area):
 	var result = []
-	for unit in $Units.get_children():
-		if (unit.global_position.x > area[0].x) and (unit.global_position.x < area[1].x):
-			if (unit.global_position.y > area[0].y) and (unit.global_position.y < area[1].y):
-				result.append(unit)
+	for unit in get_children():
+		if unit.is_in_group("unit"):
+			if (unit.global_position.x > area[0].x) and (unit.global_position.x < area[1].x):
+				if (unit.global_position.y > area[0].y) and (unit.global_position.y < area[1].y):
+					result.append(unit)
 	return result
 
 func _on_camera_area_selected(object):
@@ -23,7 +24,8 @@ func _on_camera_area_selected(object):
 	
 	# If player is pressing ctrl, don't deselect already selected units 
 	if (!is_Ctrl_pressed):
-		for unit in $Units.get_children():
-			unit.set_selected(false)
+		for unit in get_children():
+			if unit.is_in_group("unit"):
+				unit.set_selected(false)
 	for u in selected_units: # Select the units in selection box
 		u.set_selected(true)
