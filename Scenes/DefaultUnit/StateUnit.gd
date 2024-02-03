5c26c62e2cc0c2ce480120c966a7f8c687314d2a
AlexM <134806248+oceanbytes12@users.noreply.github.com>
2024-02-02 23:09:07 -0800

    Fixed Weird Unit facing bug

--- Scenes/DefaultUnit/StateUnit.gd
+++ Scenes/DefaultUnit/StateUnit.gd
@@ -49,12 +49,14 @@ func _process(_delta):
 		$MovementAnimations.play("Walk")
 	elif velocity.length() > walkSpeed and not is_dead:
 		$MovementAnimations.play("WalkFast")
-	if velocity.x < 0:
-		$Art/Body.flip_h = true
-		$Art/Head.flip_h = true
-	elif velocity.x > 0:
-		$Art/Body.flip_h = false
-		$Art/Head.flip_h = false
+	if $SM.current_target:
+		var target_vector = $SM.current_target.global_position - global_position
+		if target_vector.x < 0:
+			$Art/Body.flip_h = true
+			$Art/Head.flip_h = true
+		elif target_vector.x > 0:
+			$Art/Body.flip_h = false
+			$Art/Head.flip_h = false
 
 func _physics_process(_delta):
 	move_and_slide()
