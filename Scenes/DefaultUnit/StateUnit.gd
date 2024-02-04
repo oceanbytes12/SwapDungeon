5d29a33412a31435fd4e6bfa171226f24aa0557e
meginos <44211195+meginos@users.noreply.github.com>
2024-02-04 15:08:33 -0800

    Added standard unit animations back

--- Scenes/DefaultUnit/StateUnit.gd
+++ Scenes/DefaultUnit/StateUnit.gd
@@ -44,12 +44,14 @@ func _ready():
 	set_targeted(targeted)
 
 func _process(_delta):
-	#if velocity.length() < 1 and not is_dead:
-		#$MovementAnimations.play("RESET")
-	#elif velocity.length() < walkSpeed + 1 and not is_dead:
-		#$MovementAnimations.play("Walk")
-	#elif velocity.length() > walkSpeed and not is_dead:
-		#$MovementAnimations.play("WalkFast")
+	# Quick fix to add back unit animations without affecting bosses
+	if can_be_stunned:
+		if velocity.length() < 1 and not is_dead:
+			$MovementAnimations.play("RESET")
+		elif velocity.length() < walkSpeed + 1 and not is_dead:
+			$MovementAnimations.play("Walk")
+		elif velocity.length() > walkSpeed and not is_dead:
+			$MovementAnimations.play("WalkFast")
 	if $SM.current_target:
 		var target_vector = $SM.current_target.global_position - global_position
 		if target_vector.x < 0:
