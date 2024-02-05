a86c38e1615eb00c71fbb1f9f6d56f9b0461d377
AlexM <134806248+oceanbytes12@users.noreply.github.com>
2024-02-04 20:57:46 -0800

    Fixed Rogue sound error

---
+++ Scenes/Attacks/SmolSlashAttack/SmolSlashAttack2.gd
@@ -0,0 +1,20 @@
+extends AnimatedSprite2D
+
+var source_team_color
+var target
+var damage = 20
+
+
+func _on_animation_finished():
+	#$Sword_whoosh_sfx.play()
+	queue_free()
+
+
+func _on_area_2d_body_entered(body):
+	# Check if hitting self or friend
+	if body.is_in_group("unit") and body.teamColor != source_team_color:
+		if body.has_method("take_hit"):
+
+			body.take_hit(global_position, damage) 
+			$Rogue_stab_sfx.play()
+		
